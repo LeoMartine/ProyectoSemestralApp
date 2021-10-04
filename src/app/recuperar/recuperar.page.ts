49679874e9173a3a9e0713fc685bbc547e2a95e9
Leonardo Martinez <leo.martinezp@duocuc.cl>
2021-10-03 21:19:19 -0300

    recuperacion de contraseña funcionando

--- src/app/recuperar/recuperar.page.ts
+++ src/app/recuperar/recuperar.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recuperar',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarPage implements OnInit {
 
-  constructor() { }
+  recupe: FormGroup;
+  constructor(private router: Router, public fb: FormBuilder,
+    public alertController: AlertController) {
+      this.recupe = this.fb.group({
+        'nombre': new FormControl("",Validators.required),
+        'correo': new FormControl("",Validators.required)
+      });
+    }
 
   ngOnInit() {
   }
 
+  async mostrar()
+  {
+    var mos = this.recupe.value;
+    var usuario = JSON.parse(localStorage.getItem('usuario'));
+    if(usuario.nombre == mos.nombre && usuario.correo == mos.correo)
+    {
+      const alert = await this.alertController.create({
+        message: 'Estimado '+usuario.nombre+' se ha enviado su contraseña al correo',
+        buttons: ['Aceptar']
+      })
+      await alert.present();
+      this.router.navigateByUrl('menu');
+    }
+    else
+    {
+      const alert = await this.alertController.create({
+        header: 'Datos incorrectos',
+        message: 'Los datos que ingresaste son incorrectos.',
+        buttons: ['Aceptar']
+      })
+      await alert.present();
+    }
+  }
 }
