d8395c6a75701bc069cad2013e328a7f2cbd2477
Leonardo Martinez <leo.martinezp@duocuc.cl>
2021-11-13 22:36:11 -0300

    asdf

--- src/app/recuperar/recuperar.page.ts
+++ src/app/recuperar/recuperar.page.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { ActivatedRoute, NavigationExtras } from '@angular/router';
+import { ApiTokenService } from '../servicios/api-token.service';
 
 
 @Component({
@@ -11,22 +12,26 @@ import { ActivatedRoute, NavigationExtras } from '@angular/router';
   styleUrls: ['./recuperar.page.scss'],
 })
 export class RecuperarPage implements OnInit {
-
+  showPassword= false;
+  passwordToggleIcon = 'eye';
   recupe: FormGroup;
   usuar1: any = {
     mail1:''
   };
   constructor(private router: Router, public fb: FormBuilder,
-    public alertController: AlertController) {
+    public alertController: AlertController,
+    private activateRoute: ActivatedRoute, 
+    private api: ApiTokenService) {
       this.recupe = this.fb.group({
-        'correo': new FormControl("",Validators.required)
+        'correo': new FormControl("",Validators.required),
+        'password': new FormControl("",Validators.required)
       });
     }
 
   ngOnInit() {
   }
 
-  async mostrar()
+  async mostrar() //Cambio de Pass basado en LocalStorage
   {
     var mos = this.recupe.value;
     var usuario = JSON.parse(localStorage.getItem(mos.correo));
@@ -53,6 +58,38 @@ export class RecuperarPage implements OnInit {
         buttons: ['Aceptar']
       })
       await alert.present();
+    }
+  }
+
+  mostrar2() //Cambio de Pass basado en API
+  {
+    var mos = this.recupe.value;
+    var recu = {
+      "correo": mos.correo,
+      "nueva_password": mos.password,
+      "token_equipo": 1000300180
+    }
+    this.api.postModificar(recu).subscribe(async(resultado)=>
+    {
+      console.log(resultado);
+      const alert = await this.alertController.create({
+        header: 'Datos correctos',
+        buttons: ['Aceptar']
+      })
+      await alert.present();
+      this.router.navigate(['/login']);
+    })
+  }
+
+  togglePassword():void{
+    this.showPassword =!this.showPassword;
+    if(this.passwordToggleIcon == 'eye')
+    {
+      this.passwordToggleIcon = 'eye-off';
+    }
+    else
+    {
+      this.passwordToggleIcon = 'eye';
     }
   }
 }
